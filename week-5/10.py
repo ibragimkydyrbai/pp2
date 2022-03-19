c23aa5c9c02e30886260cf948669b39dd7b03fcb
ibragimkydyrbai <ibragimkydyrbaj750@gmail.com>
2022-03-19 16:26:08 +0600

    https://github.com/ibragimkydyrbai/pp2/tree/main/week5

--- week-5/10.py
+++ week-5/10.py
@@ -1,8 +1,8 @@
-from re import x
+from re import sub
 def a(b):
   return '_'.join(
-    x('([A-Z][a-z]+)', r' \1',
-    x('([A-Z]+)', r' \1',
+    sub('([A-Z][a-z]+)', r' \1',
+    sub('([A-Z]+)', r' \1',
     b.replace('-', ' '))).split()).lower()
 
 print(a(input()))
